7b40c05d0ba6af7a50ec195e4136c32ef025737d
KyAnhVo <tommyvo0406@gmail.com>
2024-03-29 19:18:58 -0500

    Update

--- Display.asm
+++ Display.asm
@@ -1,10 +1,14 @@
-.globl printString
+.globl printString, strDisplay
+
+.data
+strDisplay	.asciiz		"|x|x|x|x|x|"
 
 # Print string
 # Input:
-#	- $a0: address to first char of null-terminated string to print
-# 
-# 
+# $a0: address to first char of null-terminated string to print
+# Output:
+# $v0: str
 
-printString:
-	
+getStrDisplay:
+	la	$v0,	strDisplay
+	lw	$t0,	
