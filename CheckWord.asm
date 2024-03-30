09d843b0cb6ee260c553311d0285f16c2cbec034
KyAnhVo <tommyvo0406@gmail.com>
2024-03-29 23:00:05 -0500

    Check word update

--- CheckWord.asm
+++ CheckWord.asm
@@ -1,164 +1,170 @@
+.globl		checkWord
+
 .data
-correct_word:         .asciiz "hello"         # Null-terminated correct word
-msg_prompt:           .asciiz "\nEnter your guess (5 characters): "
-msg_correct:          .asciiz "Congratulations! You guessed correctly.\n"
-msg_wrong:            .asciiz "Sorry, your guess is incorrect.\n"
-msg_feedback:         .asciiz "Feedback: "
-correct_position_msg: .asciiz "Letters in correct position: "
-wrong_position_msg:   .asciiz "\nLetters in wrong position: "
-not_in_word_msg:      .asciiz "\nLetters not in the word: "
-guess:                .space 6
-feedback:             .space 20
-newline:              .asciiz "\n"            # Newline character
+rightPos:	.byte		94		# ^
+wrongPos:	.byte		42		# *
+noChar:		.byte		88		# X
+answerStr:	.asciiz		"XXXXX"		# For answer
+
+# from here are datas for debug.
+debugCorr:	.asciiz		"realm"
+debugInp:	.asciiz		"raile"
+debugMssg1:	.asciiz		"Before"
+debugMssg2:	.asciiz		"After"
 
 .text
-.globl main
 
-main:
-    li $t0, 6                           # Initialize guess count to 6
+# obviously debug function.
+mainDebug:
+	la	$s0,	debugInp
+	la	$s1,	debugCorr
+	move	$a0,	$s0
+	move	$a1,	$s1
+	jal	checkWord
+	move	$s2,	$v0
+	
+	move	$a0,	$s1
+	li	$v0,	4
+	syscall
+	
+	li	$a0,	10
+	li	$v0,	11
+	syscall
+	
+	move	$a0,	$s0
+	li	$v0,	4
+	syscall
+	
+	li	$a0,	10
+	li	$v0,	11
+	syscall
+	
+	move	$a0,	$s2
+	li	$v0,	4
+	syscall
+	
+	li	$a0,	10
+	li	$v0,	11
+	syscall
+	
+	li	$v0,	10
+	syscall
+	
 
-get_guess:
-    # Print prompt
-    li $v0, 4
-    la $a0, msg_prompt
-    syscall
-
-    # Read user input
-    li $v0, 8
-    la $a0, guess
-    li $a1, 6                           # Maximum number of characters to read (including null terminator)
-    syscall
-
-    # Compare the guess with the correct word
-    la $t1, correct_word                # Load address of the correct word
-    la $t2, guess                       # Load address of the user's guess
-    li $t3, 0                           # Initialize index
-
-# checkWord:
+# checkWord: Gets 2 words, check wordle correspondence
 #
 # Input
-# - $a0: address of nullterminated string of length 5 for correct word
-# - $a1: address of nullterminated string of length 5 for user-input word
+# - $a0: address of nullterminated string of length 5 for input word
+# - $a1: address of nullterminated string of length 5 for correct word
 # Output:
 # - $v0: address of ullterminated string of length 5 that indicates the result where
 #  	^: correct position
 #	*: in word, wrong position
 #	X: not in word
+#
+#
+# Idea: (Assume $a0 is inputWord and $a1 is correctWord)
+# char* checkWord(char* inputWord, char* correctWord) {
+# |	char* answerStr = new char; // just for syntax purpose
+# |	for (int i = 0; i < 5; i++) {			F1
+# |	|	int notInWord = 1;
+# |	|	t0 = correctWord + i;
+# |	|	if (*inputWord == *t0) {		I1
+# |	|	|	*answerStr = '^';
+# |	|	|	continue;
+# |	|	}
+# |	|	for (int j = 0; j < 5; j++) {		F2
+# |	|	|	t1 = correctWord + j;
+# |	|	|	if (*inputWord == *t1) {	I2
+# |	|	|	|	*answerStr = '*';
+# |	|	|	|	notInWord = 0;
+# |	|	|	|	break;
+# |	|	|	}
+# |	|	}
+# |	|	if (notInWord)				I3
+# |	|		*answerStr = 'X';
+# |	|	inputWord = inputWord + 1;
+# |	|	answerStr = answerStr + 1
+# |	}
+# |	return answerStr;
+# }
+#
+# Let $s0 be i, $s1 be j, $s2 be notInWord, and $s3 be 5 (loop comparision stuff)
+# Note: This is pass by value so registers $a0 and $a1 WILL be changed.
 
-# aabbc
-# bacde
-# *^*XX
+checkWord:
 
-compare_loop:
-    lb $s4, 0($t2)                     # Load character from guess
-    beq $s4, $zero, end_comparison      # If end of string, exit loop
+	# Subroutine Stuffs
+	addi	$sp,	$sp,	-24
+	sw	$s0,	0($sp)
+	sw	$s1,	4($sp)
+	sw	$s2,	8($sp)
+	sw	$s3,	12($sp)
+	sw	$t0,	16($sp)
+	sw	$t1,	20($sp)
+	
+	# Let $s0 be i, $s1 be j, $s2 be notInWord, and $s3 be 5 (loop comparision stuff)
+	add	$s0,	$zero,	$zero
+	add	$s1,	$zero,	$zero
+	addi	$s2,	$zero,	1		# int notInWord = 1;
+	addi	$s3,	$zero,	5
+	
+	#load address of answerStr into return reg $v0
+	la	$v0,	answerStr
+	
+L1:	# Outside loop
+	addiu	$s2,	$zero,	1
+	add	$t1,	$a1,	$s0
+	lb	$t0,	($a0)
+	lb	$t1,	($t1)
+	bne	$t1,	$t0,	I1F
+I1T:	lb	$t0,	rightPos
+	sb	$t0,	($v0)
+	j	L1Cond
+I1F:	add	$s1,	$zero,	$zero	# reset loop condition for inner loop
 
-    lb $s5, 0($t1)                     # Load character from correct word
-    beq $s5, $zero, wrong_guess         # If end of string, jump to wrong_guess
+L2:	# Inside loop
+	add	$t1,	$a1,	$s1
+	lb	$t1,	($t1)
+	lb	$t0,	($a0)
+	bne	$t0,	$t1,	I2F
+I2T:	lb	$t0,	wrongPos
+	sb	$t0,	($v0)
+	add	$s2,	$zero,	$zero
+	j	L2E
+I2F:	nop
+L2Cond:	addi	$s1,	$s1,	1
+	bne	$s1,	$s3,	L2
+	
+L2E:	# Inner loop end
+	beq	$s2,	$zero,	I3F
+I3T:	lb	$t0,	noChar
+	sb	$t0,	($v0)
+I3F:	nop
+L1Cond:	addi	$a0,	$a0,	1
+	addi	$v0,	$v0,	1
+	addi	$s0,	$s0,	1
+	bne	$s0,	$s3,	L1
+	
+L1E:	# End outside loop
+	la	$v0,	answerStr
 
-    bne $s4, $s5, check_next            # If characters don't match, check next character
-    addi $t1, $t1, 1                   # Move to next character in correct word
-    addi $t2, $t2, 1                   # Move to next character in guess
-    addi $t3, $t3, 1                   # Increment index
-    j compare_loop                     # Repeat for next character
-
-check_next:
-    li $t6, 0                           # Initialize flag for wrong position
-    move $t7, $t3                       # Copy index to $t7
-
-    # Check if the current character in guess exists in the correct word
-check_next_loop:
-    slti $t8, $t7, 5                    # Check if index is less than 5 (length of correct word)
-    beq $t8, $zero, not_in_word         # If index exceeds length, jump to not_in_word
-
-    lb $t8, 0($t2)                     # Load character from guess
-    lb $t9, 0($t1)                     # Load character from correct word
-    beq $t9, $zero, not_in_word         # If end of correct word, jump to not_in_word
-    bne $t8, $t9, next_char             # If characters don't match, check next character
-
-    sb $t8, feedback($t3)               # Store character in feedback buffer
-    addi $t3, $t3, 1                    # Increment index for feedback buffer
-
-next_char:
-    addi $t7, $t7, 1                    # Increment counter
-    addi $t1, $t1, 1                    # Move to next character in correct word
-    j check_next_loop                   # Repeat loop
-
-not_in_word:
-    li $t6, 1                            # Set flag for wrong position
-
-wrong_guess:
-    # Print feedback
-    li $v0, 4
-    la $a0, msg_feedback
-    syscall
-
-    li $v0, 4
-    la $a0, feedback
-    syscall
-
-    # Print each guess feedback on a new line
-    li $v0, 4
-    la $a0, newline
-    syscall
-
-    # Print "correct position" feedback
-    li $v0, 4
-    la $a0, correct_position_msg
-    syscall
-    li $v0, 11
-    la $a0, ($s5)
-    syscall
-    	
- 
-    # Print "wrong position" feedback
-    li $v0, 4
-    la $a0, wrong_position_msg
-    syscall
-    li $v0, 11
-    la $a0, ($s4)
-    syscall
-    
-    # Print "not in word" feedback
-    li $v0, 4
-    la $a0, not_in_word_msg
-    syscall
-    li $v0, 11
-    la $a0, ($s4)
-    syscall
-    
-    # Decrement guess count
-    addi $t0, $t0, -1
-    beqz $t0, out_of_guesses
-
-    # Repeat loop for next guess
-    j get_guess
-
-end_comparison:
-    # Print feedback
-    li $v0, 4
-    la $a0, msg_feedback
-    syscall
-
-    li $v0, 4
-    la $a0, feedback
-    syscall
-
-    # Print correct message
-    li $v0, 4
-    la $a0, msg_correct
-    syscall
-
-    # Exit program
-    li $v0, 10
-    syscall
-
-out_of_guesses:
-    # Print message for running out of guesses
-    li $v0, 4
-    la $a0, msg_wrong
-    syscall
-
-    # Exit program
-    li $v0, 10
-    syscall
+	# Subroutine stuffs
+	lw	$s0,	0($sp)
+	lw	$s1,	4($sp)
+	lw	$s2,	8($sp)
+	lw	$s3,	12($sp)
+	lw	$t0,	16($sp)
+	lw	$t1,	20($sp)
+	addi	$sp,	$sp,	24
+	jr	$ra
+	
+	
+# Note on for/if label notation:
+# For:
+# 	Ln:  	for loop #n
+#	LnE: 	end for loop #n
+#	LnCond:	update condition and check for for loop #n
+# If:
+# 	InT: if #n is true
+#	InF: if #n is false
